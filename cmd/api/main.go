cf479b96e83286fc435a0f1922a8c49fc97e26dc
vitaliiPsl <vitalii.psl.main@gmail.com>
2024-04-07 20:59:19 +0300

    Create Fiber server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"vitaliiPsl/synthesizer/internal/logger"
+	"vitaliiPsl/synthesizer/internal/server"
+)
+
 func main() {
-	
+	logger.Logger.Info("Spinning up Synthesizer...")
+
+	server := server.New()
+
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	err := server.Listen(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
 }
