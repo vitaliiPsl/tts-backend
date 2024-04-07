ac948d4a6ba9f86b2424732effbfddcf051a6bb8
vitaliiPsl <vitalii.psl.main@gmail.com>
2024-04-07 21:17:57 +0300

    Create sign up request and validator

---
+++ internal/requests/auth_requests.go
@@ -0,0 +1,7 @@
+package requests
+
+type SignUpRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
---
+++ internal/validation/validation_service.go
@@ -0,0 +1,62 @@
+package validation
+
+import (
+	"fmt"
+	"unicode"
+
+	"github.com/go-playground/validator/v10"
+	service_errors "vitaliiPsl/synthesizer/internal/error"
+	"vitaliiPsl/synthesizer/internal/logger"
+	"vitaliiPsl/synthesizer/internal/requests"
+)
+
+const MinPasswordLength = 8
+
+type ValidationService struct {
+	v *validator.Validate
+}
+
+func NewValidationService() *ValidationService {
+	return &ValidationService{
+		v: validator.New(),
+	}
+}
+
+func (vs *ValidationService) ValidateSignUpRequest(request *requests.SignUpRequest) error {
+	if err := vs.v.Struct(request); err != nil {
+		logger.Logger.Error("Request didn't pass validation", "message", err.Error())
+		return &service_errors.ErrBadRequest{Message: "Request didn't pass validation"}
+	}
+
+	return validatePassword(request.Password)
+}
+func validatePassword(password string) error {
+	var hasUpper, hasLower, hasNumber, hasSpecial bool
+
+	if len(password) < MinPasswordLength {
+		return &service_errors.ErrBadRequest{
+			Message: fmt.Sprintf("password must be at least %d characters long", MinPasswordLength),
+		}
+	}
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsDigit(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	if !(hasUpper && hasLower && hasNumber && hasSpecial) {
+		return &service_errors.ErrBadRequest{
+			Message: "Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character",
+		}
+	}
+
+	return nil
+}
